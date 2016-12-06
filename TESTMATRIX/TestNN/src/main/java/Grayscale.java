6da17719d0a2d140b4672d28267fe90f8b7d7402
DYEWolf <angel.rmarin@outlook.com>
2016-12-05 22:25:38 -0600

    Add files via upload

--- TESTMATRIX/TestNN/src/main/java/Grayscale.java
+++ TESTMATRIX/TestNN/src/main/java/Grayscale.java
@@ -12,28 +12,28 @@ public class Grayscale {
         BufferedImage img = null;
         File f = null;
         int count=0;
-        int sum=0;
-        int j,c=0;
-        int i = 0;
-        int LimitA = 3;
-        int LimitB = 3;
+        double sum=0;
+        int j;
+        int k;
+        int CounterA = 0;
+        int CounterB = 0;
         int l,m=0;
-        int v=0;
-        int w;
         int holderX;
         int holderY;
-        int holderV=0;
+        int holderV;
         int cunt=0;
+        int cycle=0;
+        int w=0,v=0;
+
 
 
         BufferedImage edge = null;
         File edgeimg = null;
 
 
-
         //read image
         try{
-            f = new File("/home/cristopher_ramirez/Documents/TESTMATRIX/bear.jpg");
+            f = new File("/home/cristopher_ramirez/Documents/TESTMATRIX/wey.jpg");
             img = ImageIO.read(f);
         }catch(IOException e){
             System.out.println(e);
@@ -43,15 +43,17 @@ public class Grayscale {
         int width = img.getWidth();
         int height = img.getHeight();
 
-        int matrix[][] = new int[width][height];
-        int edgeMatrix[][] = {
+        int ImageMatrix[][] = new int[width][height];
+        int Kernel[][] = {
 
                 {-1,-1,-1},
                 {-1,8,-1},
                 {-1,-1,-1}
 
         };
-        int ConEdgeMatrix[][] = new int[width][height];
+
+        int ConvEdgeMatrix[][] = new int[width][height];
+        int output[][] = new int[width][height];
 
 
         //convert to grayscale
@@ -70,11 +72,11 @@ public class Grayscale {
                 //replace RGB value with avg
                 p = (a<<24) | (avg<<16) | (avg<<8) | avg;
 
-                matrix[x][y]=avg;
+                ImageMatrix[x][y]=avg;
 
 
                 //Printing the pixel value
-                System.out.print(matrix[x][y] + " ");
+                System.out.print(ImageMatrix[x][y] + " ");
                 //Counter for the pixels
                 count++;
 
@@ -82,31 +84,50 @@ public class Grayscale {
             }
             System.out.println();
         }
+        System.out.println("Ancho:" + width);
+        System.out.println("Largo:" + height);
         System.out.println("Numero de pixeles en la imagen:" + count);
         count=0;
 
 
         //function to get the convolved feature (multiply the matrix and the edgematrix)
 
-        for(int y=0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                w = 0;
-                holderX = x;
-                for (w = 0; w < 3; w++) {
-                    v = 0;
-                    if (w > 0)
-                        x++;
-                    for (v = 0; v < 3; v++) {
-                        sum = sum + matrix[v][x] * edgeMatrix[v][w];
-                        if (w == 2 && v == 2)
-                            x = holderX;
-                    }
+       //for(j=0;j<height;j++) {
+           for (k = 0; k < width; k++) {
+
+               for (w = 0; w < 3; w++) {
+                   int Sj=w;
+                   for (v = 0; v < 3; v++) {
+                       int Sk=v+CounterB;
+                       sum += ImageMatrix[Sj][Sk] * Kernel[w][v];
+                   }
+               }
+               CounterA++;
+               CounterB++;
+           }
+       //}
+
+
+
+   /*     holderY=cycle;
+        for (int x = 0; x < width; x++) {
+            w = 0;
+            holderX = x;
+            for (w = 0; w < 3; w++) {
+                v = 0;
+                if (w > 0)
+                    x++;
+                for (v = 0; v < 3; v++) {
+                    sum = sum + ImageMatrix[v][x] * Kernel[v][w];
+                    if (w == 2 && v == 2)
+                        x = holderX;
                 }
-                System.out.println("Valor final de la matriz:" + sum);
-                cunt++;
             }
-            System.out.println("Numero de salidas de la matriz:" + cunt);
+            System.out.println("Valor final de la matriz:" + sum);
+            cunt++;
         }
+        System.out.println("Numero de salidas de la matriz:" + cunt);
+*/
 
 
 
